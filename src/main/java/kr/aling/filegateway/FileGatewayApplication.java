900c14d9fa189c192f8c0b69d81b363609c16d7a
Gyeongseo Bak <rudtj2966@gmail.com>
2024-02-01 13:51:33 +0900

    [Docs] : 주석 작성

--- src/main/java/kr/aling/filegateway/FileGatewayApplication.java
+++ src/main/java/kr/aling/filegateway/FileGatewayApplication.java
@@ -2,8 +2,16 @@ package kr.aling.filegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+/**
+ * File Gateway Application.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ */
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class FileGatewayApplication {
 
 	public static void main(String[] args) {
